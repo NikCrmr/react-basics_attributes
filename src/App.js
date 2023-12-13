7d073c09c253832a46ca30e20d440e0a66b8e6ce
Niko Cremer <n.ko@posteo.de>
2023-12-13 11:07:19 +0100

    exercise done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,46 @@
 import React from "react";
 import "./styles.css";
 
-export default function App() {
-  return <div>Replace me with an article</div>;
+function Article() {
+  return (
+    <article className="article">
+      <h1 className="article__title">Heading</h1>Article{" "}
+      <label htmlFor="input">I am a label for the input</label>
+      <input id="input" placeholder="put your text here..."></input>
+      <a href="https://create-react-app.dev" className="article__link">
+        Link to Create React App
+      </a>
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+        ipsum dolor sit amet.
+      </p>
+    </article>
+  );
 }
+
+export default function App() {
+  return (
+    <>
+      <Article />
+    </>
+  );
+}
+
+/*
+- an `<article>` as a wrapper HTML element with the class `article`
+- an `<h2>` with the class `article__title` and a text of your choice
+- a `<label>` and `<input>` tag connected with `id` and `htmlFor` (!) attributes
+- an `<a>` tag with the class `article__link` as well as text content and `href` attribute of your choice (What about a Wikipedia article?)
+
+## Notes
+
+- You rendered Article component does not have any styling? Remember that you need to use `className` instead of `class` in JSX.
+- You only have to touch the [App.js](src/App.js) file.
+*/
